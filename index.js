8b0ac2b053e9716a1ab93f704a5f3ffa2c3aa0c3
Khushal Suthar <khushal9587@gmail.com>
2023-09-05 18:35:18 +0530

    Add Decimal Functionality

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const plusElement = document.getElementById("plus");
 const dotElement = document.getElementById("dot");
 const equalsElement = document.getElementById("equals");
 
-const numberElementArray = [zeroElement, oneElement, twoElement, threeElement, fourElement, fiveElement, sixElement, sevenElement, eightElement, nineElement, percentageElement, divideElement, multiplyElement, subtractElement, plusElement];
+const numberElementArray = [zeroElement, oneElement, twoElement, threeElement, fourElement, fiveElement, sixElement, sevenElement, eightElement, nineElement, percentageElement, divideElement, multiplyElement, subtractElement, plusElement, dotElement];
 
 let inputNumberArray = [];  //Stores array of digits
 
@@ -28,9 +28,13 @@ let wholeNumberArray = [];  //Stores all the input numbers
 
 let wholeNumber;            //Stores a full number
 
-let number = 0;             //Stores the number just to display it
+let number = 0;             //Stores the number operandIndexust to display it
 
 let inputString = "0";
+
+let operatorArray = [];
+
+let flag = 0;
 
 
 //Add event listener to all the numbered buttons
@@ -46,12 +50,32 @@ numberElementArray.forEach((e)=>{
 
 //Add Number according to place
 function contNumber(value){
+
+    let lastInputValue = inputString[inputString.length - 1];
+
     if(!isNaN(value) && inputString === "0"){
         inputString = "";
     }
-    if((inputString[inputString.length - 1] === "/" || inputString[inputString.length - 1] === "x" || inputString[inputString.length - 1] === "%" || inputString[inputString.length - 1] === "+" || inputString[inputString.length - 1] === "-") && (value === "x" || value === "/" || value === "%" || value === "+" || value === "-")){
+
+    if(value === "."){
+        if(flag === 0){
+            inputString = inputString.concat(value);
+            resultElement.innerText = inputString;
+            flag = 1;
+            return;
+        }else if(value === "."){
+            return;
+        }
+    }
+
+    if(value === "x" || value === "/" || value === "%" || value === "+" || value === "-"){
+        flag = 0;
+    }
+
+    if((lastInputValue === "/" || lastInputValue === "x" || lastInputValue === "%" || lastInputValue === "+" || lastInputValue === "-") && (value === "x" || value === "/" || value === "%" || value === "+" || value === "-")){
         return;
     }
+
     inputString = inputString.concat(value);
     resultElement.innerText = inputString;
 }
@@ -60,77 +84,115 @@ function contNumber(value){
 clearElement.addEventListener('click', ()=>{
     resultElement.innerText = "0";
     inputString = "0";
+    operatorArray = [];
 })
 
 equalsElement.addEventListener('click', ()=>{
     let number = 0;
+    let decimalNumber = 0;
+    let decimalPlaces = 1;
     let resultNumber = 0;
     let numberArray = [];
-    let i = 0;
-    let j = 0;
-    while(i < inputString.length){
+    let index = 0;
+    let operandIndex = 0;
+    let toggleDecimal = 0;
+    console.log(inputString);
 
-        while(!isNaN(inputString[i])){
-            number = (number*10) + Number(inputString[i]);
-            i++;
-            j++;
+    while (operandIndex < inputString.length){
+        if(isNaN(inputString[operandIndex])){
+            if(inputString[operandIndex] !== "."){
+                operatorArray.push(inputString[operandIndex]);
+                operandIndex++;
+            }else{
+                operandIndex++;
+            }
+        }
+        operandIndex++;
+    }
+
+    operandIndex = 0;
+    
+    while(index < inputString.length){
+
+        let toggleDecimal = 0;
+        decimalPlaces = 1;
+
+        while(!isNaN(inputString[index]) || inputString[index] === '.'){
+            if(inputString[index] === '.'){
+                toggleDecimal = 1;
+                index++;
+            }
+            if(toggleDecimal){
+                decimalNumber += Number(inputString[index])/Math.pow(10, decimalPlaces);
+                console.log({decimalNumber});
+                decimalPlaces++;
+                index++;
+                continue;
+            }
+            number = (number*10) + Number(inputString[index]);
+            index++;
         }
 
-        if(isNaN(inputString[j])){
-            value = inputString[j];
-            console.log({value});
-        }
-        
+        number = number + decimalNumber;
+        decimalNumber = 0;
         numberArray.push(number);
         number = 0;
-        i++;
-
-        if(numberArray.length === 2 && value === "+"){
-            resultNumber = 0;
-            numberArray.forEach((e)=>{
-                resultNumber += e;
-            });
-            numberArray = [];
-            numberArray.push(resultNumber);
-            console.log({numberArray});
-        }
+        console.log({numberArray});
+        index++;
         
-        if(numberArray.length === 2 && value === "x"){
-            resultNumber = 1;
-            numberArray.forEach((e)=>{
-                resultNumber *= e;
-            });
-            numberArray = [];
-            numberArray.push(resultNumber);
-            console.log({numberArray});
-        }
+        if(numberArray.length === 2){
+            console.log(operatorArray[operandIndex]);
+            switch(operatorArray[operandIndex]){
+                case "+":
+                    resultNumber = 0;
+                    numberArray.forEach((e)=>{
+                        resultNumber += e;
+                    });
+                    numberArray = [];
+                    numberArray.push(resultNumber);
+                    console.log({numberArray});
+                    break;
 
-        if(numberArray.length === 2 && value === "/"){
-            resultNumber = 1;
-            resultNumber = numberArray[0]/resultNumber;
-            resultNumber = resultNumber/numberArray[1];
-            numberArray = [];
-            numberArray.push(resultNumber);
-            console.log({numberArray});
-        }
+                case "-":
+                    resultNumber = 0;
+                    resultNumber += numberArray[0];
+                    resultNumber = resultNumber - numberArray[1];
+                    numberArray = [];
+                    numberArray.push(resultNumber);
+                    console.log({numberArray});
+                    break;
 
-        if(numberArray.length === 2 && value === "-"){
-            resultNumber = 0;
-            resultNumber += numberArray[0];
-            resultNumber = resultNumber - numberArray[1];
-            numberArray = [];
-            numberArray.push(resultNumber);
-            console.log({numberArray});
-        }
+                case "x":
+                    resultNumber = 1;
+                    numberArray.forEach((e)=>{
+                        resultNumber *= e;
+                    });
+                    numberArray = [];
+                    numberArray.push(resultNumber);
+                    console.log({numberArray});
+                    break;
 
-        if(numberArray.length === 2 && value === "%"){
-            resultNumber = 0;
-            resultNumber = numberArray[0];
-            resultNumber = resultNumber % numberArray[1];
-            numberArray = [];
-            numberArray.push(resultNumber);
-            console.log({numberArray});
+                case "/":
+                    resultNumber = 1;
+                    resultNumber = numberArray[0];
+                    resultNumber = resultNumber/numberArray[1];
+                    numberArray = [];
+                    numberArray.push(resultNumber);
+                    console.log({numberArray});
+                    break;
+
+                case "%":
+                    resultNumber = 0;
+                    resultNumber = numberArray[0];
+                    resultNumber = resultNumber % numberArray[1];
+                    numberArray = [];
+                    numberArray.push(resultNumber);
+                    console.log({numberArray});
+            }
+            operandIndex++;
         }
     }
+
+    console.log({operatorArray});
     resultElement.innerText = resultNumber;
 });
